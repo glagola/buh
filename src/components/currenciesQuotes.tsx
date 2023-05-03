607fb5d201f786a693d673095cf9b75b04491435
Igor Glagola <igor@glagola.ru>
2023-05-03 12:00:22 +0000

    Add required currencies quotes

--- src/components/currenciesQuotes.tsx
+++ src/components/currenciesQuotes.tsx
@@ -7,16 +7,23 @@ import { isNonEmpty } from '@/utils/array';
 import ExpressionInput from './expressionInput';
 import { type THistoryItemForm } from './form';
 
+const requiredCurrencies: TCurrency[] = [{ isoCode: 'USD' }, { isoCode: 'RUB' }];
+
 const CurrenciesQuotes = () => {
     const form = useFormContext<THistoryItemForm>();
     useWatch({ control: form.control });
 
-    const currencies = Object.values(form.getValues('accounts')).reduce<TCurrency[]>((res, states) => {
-        if (isNonEmpty(states)) {
-            res.push(states[0].account.currency);
-        }
-        return res;
-    }, []);
+    const unique = new Set(requiredCurrencies.map((item) => item.isoCode));
+
+    const currencies = Object.values(form.getValues('accounts')).reduce<TCurrency[]>(
+        (res, states) => {
+            if (isNonEmpty(states) && !unique.has(states[0].account.currency.isoCode)) {
+                res.push(states[0].account.currency);
+            }
+            return res;
+        },
+        [...requiredCurrencies],
+    );
 
     return (
         <Stack spacing={2}>
