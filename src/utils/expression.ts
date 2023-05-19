8bd80bed56a3fc6b9812fe9114f10d09b150cd69
Igor Glagola <igor@glagola.ru>
2023-05-19 13:52:38 +0000

    Interpret comma and period as decimal separator equally

--- src/utils/expression.ts
+++ src/utils/expression.ts
@@ -1,6 +1,9 @@
 import { evaluate } from 'mathjs';
 
 export const safeEvaluate = (expr: string): number | undefined => {
+    expr = expr.replace(/\s+/g, '');
+    expr = expr.replace(/[\.,]/g, '.');
+
     try {
         return evaluate(expr.replace(/\s+/g, '')) as number;
     } catch {
